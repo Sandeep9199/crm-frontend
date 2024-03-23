65420cd0356e65102f407d56451d3c42d82092db
Sandeep kumar Sharma <57533185+Sandeep9199@users.noreply.github.com>
2024-03-23 19:28:00 +0530

    base url change

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,8 @@
 // export const BASE_URL = "https://crm-backend-navy.vercel.app/";
 // export const BASE_URL = "http://localhost:8080/";
-export const BASE_URL = "https://crmbackend-mdsg.onrender.com";
+// export const BASE_URL = "https://crmbackend-mdsg.onrender.com";
+export const BASE_URL = "https://crm-backend-oooz.onrender.com";
+
+// export const BASE_URL = "https://crmbackend-mdsg.onrender.com";
+
 
