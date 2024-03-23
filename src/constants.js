290022297a7b53568d2f37778b66068a92d761b1
Sandeep kumar Sharma <57533185+Sandeep9199@users.noreply.github.com>
2024-03-23 17:26:26 +0530

    response added index and add base url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
 // export const BASE_URL = "https://crm-backend-navy.vercel.app/";
-export const BASE_URL = "http://localhost:8080/";
+// export const BASE_URL = "http://localhost:8080/";
+export const BASE_URL = "https://crmbackend-mdsg.onrender.com";
+
